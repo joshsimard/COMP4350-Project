e1a4bff44ee944d62b7d376d7feb951bcedc4deb
edmondcotterell <edmondcotterell@gmail.com>
2016-01-28 16:20:54 -0600

    Added Navigation list to client list view issue #2

--- resources/views/clientlist.blade.php
+++ resources/views/clientlist.blade.php
@@ -1,20 +1,35 @@
 @extends('layout')
+
 @section('custom_css')
     <style>
-        body {
+        body
+        {
             background-image: url("http://books.maxoffsky.com/img/stardust.png");
         }
+
+         /*Color list background and text*/
+        .list-group-item{background-color: #dddddd;}
+        .list-group-item .items{color: #000000;}
 
     </style>
 @stop
 
+@section('header_links')
+    <ul class="nav navbar-nav">
+        <li class="active"><a href="/home"><h4>Home</h4> <span class="sr-only">(current)</span></a></li>
+        <li><a href="/calendar"><h4>Appointments</h4></a></li>
+        <li class="navbar-right"><a href="auth/logout"><h4 class="glyphicon glyphicon-log-out"></h4></a></li>
+    </ul>
+@stop
+
 @section('content')
 
-    <div class="page-header">
-        <h1>Client List</h1>
-    </div>
 
-    <div class="list-group">
+    {{--<div class="page-header">--}}
+        {{--<h2>Client List</h2>--}}
+    {{--</div>--}}
+
+    <div class="list-group col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 
      
         <?php
@@ -35,7 +50,7 @@
 
             foreach($clients as $id => $patient)
             {
-               echo '<a href="#" class="list-group-item"><h4>'.$patient.'</h4><span class="left">ID#: '.$id.'</span></a>';
+               echo '<a href="#" class="list-group-item"><h4 class="items">'.$patient.'</h4><span class="left items">ID#: '.$id.'</span></a>';
             }
 
         ?>
