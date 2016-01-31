16efbd14077df3e57d7ec26c2c2df3d63b9b25b4
edmondcotterell <edmondcotterell@gmail.com>
2016-01-31 00:01:02 -0600

    Resolved issue #2

--- resources/views/clientlist.blade.php
+++ resources/views/clientlist.blade.php
@@ -21,11 +21,16 @@
     <div class="list-group col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
 
         <?php
-            //populate
-            foreach($clients as $patient)
-            {
-               echo '<a href="#" class="list-group-item"><h4 class="items">'.$patient["firstName"].' '.$patient["lastName"].'</h4><span class="left items">ID#: '.$patient["id"].'</span></a>';
-            }
+         if(count($clients) > 1)
+             echo '<a href="#" class="list-group-item"><h4 class="items">No Clients!</h4><span class="left items"></span></a>';
+         else
+         {
+             //populate
+             foreach($clients as $patient)
+             {
+                 echo '<a href="#" class="list-group-item"><h4 class="items">'.$patient["firstName"].' '.$patient["lastName"].'</h4><span class="left items">ID#: '.$patient["id"].'</span></a>';
+             }
+         }
 
         ?>
 
