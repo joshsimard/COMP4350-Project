7b60f185402bb52300f43aa383930cc69b8b2cd1
edmondcotterell <edmondcotterell@gmail.com>
2016-02-05 11:18:03 -0600

    Added some control to calendar page

--- tests/acceptance/ClientCept.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$I = new AcceptanceTester($scenario);
-$I->wantTo('perform actions and see result');
-$I->amOnPage('/login');
-$I->fillField('email','jane@doe.com');
-$I->fillField('password','password');
-$I->click('Login');
-$I->amOnPage('/home');
-$I->canSee('view appointments');
