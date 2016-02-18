db368a12a5019ac8741e376444749089759cd50e
Kenechukwu Igweagu <kodigweagu@yahoo.com>
2016-02-18 16:17:42 -0600

    visit_form created

--- app/Http/Controllers/VisitFormEditController.php
+++ app/Http/Controllers/VisitFormEditController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class DoctorClientFormEditController extends Controller
+class VisitFormEditController extends Controller
 {
     /**
      * Create a new controller instance.
